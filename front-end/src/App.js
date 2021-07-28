c08109e4bdf8b7e2edcd36acb8b0fe3ab069ca50
Voipdbr <contatovoipd@gmail.com>
2021-07-28 13:46:50 -0300

    Primeiro Commit

---
+++ front-end/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './css/App.css'
+import Main from './Main';
+
+function App(){
+    return(
+    <>
+    <header class="">
+        <div>
+            <p class="p-1">Aplicando Api</p>
+        </div>
+    </header>
+    <Main/>
+    </>
+    )
+}
+
+export default App;
---
+++ front-end/src/Main.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './css/main.css';
+
+
+function Main(){
+    return(
+    <main>
+        <div class="d-1">
+            <input class="i-1"></input>
+        <button class="btn-1">Cadastrar</button>
+        <button class="btn-1">Editar</button>
+        <button class="btn-1">Deletar</button>
+        <button class="btn-1">Cadastrar</button>
+        </div>
+    </main>
+    )
+}
+
+export default Main;
